bbee27e4b8b059c0fbaff1c46595cd6aa4ab9a1c
leedongjin9705 <dhejrhdla@naver.com>
2020-09-18 16:20:05 +0900

    4주차 수업

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+function Food({fav}) {
+    return (
+        <h3>I love {fav}</h3>
+    );
+}
 function App() {
-  // return  <div className="App" />;    
-  return <div>Hello!!!!!</div>;
+return (
+ 
+ <div>
+    <h1>Hello</h1>
+    <Food fav="햄버거"/>
+    <Food fav="콜라"/>
+    <Food fav="치즈볼"/>
+    <Food fav="칼국수"/>
+    <Food fav="등촌"/>
+</div>
+ );
 }
 
 export default App;

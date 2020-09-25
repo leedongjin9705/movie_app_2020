ff5fab89fd48bf11ff9d8a46c091889db20bf04c
leedongjin1234 <dhejrh@daum.net>
2020-09-25 14:12:52 +0900

    로그인

--- README.md
+++ README.md
@@ -13,4 +13,4 @@ react JS fundamentails course 2020
 ## 09월 11일
 >1. Dothome에 indexfile 수정하기
 >2. readme.md 수정 후 Github에 push하기.
-* 언제까지? 9월 16일까지.  
+* 언제까지? 9월 16일까지.   
--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ function Food({fav}) {
         <h3>I love {fav}</h3>
     );
 }
+
+const foodLink = [
+{
+  name: '김치',
+},
+{
+  name: '햄버거',
+},
+{
+  name: '피자',
+},
+];
+
 function App() {
 return (
  
  <div>
     <h1>Hello</h1>
-    <Food fav="햄버거"/>
-    <Food fav="콜라"/>
-    <Food fav="치즈볼"/>
-    <Food fav="칼국수"/>
-    <Food fav="등촌"/>
-</div>
+ </div>
  );
 }
 

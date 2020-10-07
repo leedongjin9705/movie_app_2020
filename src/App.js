3fb17f6acd464277e17db302bf8636cc563c8d9a
leedongjin1234 <dhejrh@daum.net>
2020-10-07 14:20:38 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
 import React from 'react';
+import imgA from './images/1.jpg';
+import imgB from './images/2.jpg';
+import imgC from './images/3.jpg';
+import imgD from './images/4.jpg';
+import imgE from './images/5.jpg';
 
-function Food({fav}) {
-    return (
-        <h3>I love {fav}</h3>
-    );
+function Food({name, image}) {
+  return (
+    <div>
+      <h2>I love {name}</h2>
+      <img src={image} alt={name} />
+    </div>
+      );
 }
 
 const foodLink = [
-{
-  name: '김치',
-},
-{
-  name: '햄버거',
-},
-{
-  name: '피자',
-},
+  {
+    id: 1,
+    name : '김치',
+    image : imgA,
+    altText: '햄버거를 먹는 모습'
+  },
+  {
+    id: 2,
+    name: '햄버거',
+    image : imgb,
+    altText: '햄버거를 먹는 모습'
+  },
+  {
+    id: 3,
+    name: '피자',
+    image : imgC,
+    altText: '햄버거를 먹는 모습'
+  },
+  {
+    id: 4,
+    name: '피자',
+    image : imgD,
+    altText: '햄버거를 먹는 모습'
+  },
+  {
+    id: 5,
+    name: '피자',
+    image : imgE,
+    altText: '햄버거를 먹는 모습'
+  }
 ];
 
+
+
 function App() {
-return (
- 
- <div>
-    <h1>Hello</h1>
- </div>
- );
+  
+  return (
+  <div>
+    {foodLink.map(dish => (<Food key={dish.id} name = {dish.name} image={dish.image}/>))}
+  </div>
+  );
 }
 
-export default App;  
+export default App;
